06e0733660c524dfe2e179bfa83a021bb84a1e7b
ndzinovev <nfpecom@yandex.ru>
2021-12-16 12:22:24 +0500

    inital

---
+++ js/script.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+    $('.owl-carousel').owlCarousel({
+        margin:10,
+        dots:true,
+        items: 1,
+        navText: ['<img src="img/arr-l.png">',
+                '<img src="img/arr-r.png">'],
+        nav:true,
+        loop:true,
+        dots:true,
+        dotsEach: 1
+         
+    });
+
+})
+
+
+
